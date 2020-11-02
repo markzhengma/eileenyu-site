cbb94c26a5f70cfa523caef988a8e7284657a0f6
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-11-02 14:11:06 +0800

    toggle navbar button collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ export default class Header extends Component {
   render() {
     return (
       <Navbar 
+        collapseOnSelect
         bg="light" 
         fixed="top" 
         expand="sm"
@@ -13,24 +14,35 @@ export default class Header extends Component {
         <Navbar.Brand id="logo" href="/">
           Eileen Yu
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="nav-bar" />
         <Navbar.Collapse id="nav-bar">
           <Nav>
             { this.props.currPath === '/'
-              ? <Nav.Link onClick = {() => this.props.scrollToElement('cover-spacer')}>
+              ? <Nav.Link 
+                  eventKey = 'home'
+                  onClick = {() => this.props.scrollToElement('cover-spacer')}
+                >
                   主页
                 </Nav.Link>
-              : <Nav.Link as={Link} to="/">
+              : <Nav.Link 
+                  eventKey = 'home'
+                  as={Link} 
+                  to="/"
+                >
                   主页
                 </Nav.Link>
             }
             <Nav.Link 
+              eventKey = 'program'
               disabled = {this.props.currPath.match('/program/')}
               onClick = {() => this.props.scrollToElement('program-spacer')}
             >
               训练计划
             </Nav.Link>
-            <Nav.Link onClick = {() => this.props.scrollToElement('contact-spacer')}>
+            <Nav.Link 
+              eventKey = 'contact'
+              onClick = {() => this.props.scrollToElement('contact-spacer')}
+            >
               联系我
             </Nav.Link>
           </Nav>
